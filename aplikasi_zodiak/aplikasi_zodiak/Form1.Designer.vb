96b8e8f94933aad52a3240f6c0050b28dc2628e3
Individeveloper <145454651+Individeveloper@users.noreply.github.com>
2025-04-15 20:19:21 +0800

    Add files via upload

---
+++ aplikasi_zodiak/aplikasi_zodiak/Form1.Designer.vb
@@ -0,0 +1,130 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Label1 = New Label()
+        inp_name = New TextBox()
+        inp_tgl = New TextBox()
+        tgl = New Label()
+        ComboBox1 = New ComboBox()
+        Label2 = New Label()
+        Button1 = New Button()
+        ket = New Label()
+        SuspendLayout()
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Location = New Point(335, 98)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(39, 15)
+        Label1.TabIndex = 0
+        Label1.Text = "Nama"
+        ' 
+        ' inp_name
+        ' 
+        inp_name.Location = New Point(380, 95)
+        inp_name.Name = "inp_name"
+        inp_name.Size = New Size(100, 23)
+        inp_name.TabIndex = 1
+        ' 
+        ' inp_tgl
+        ' 
+        inp_tgl.Location = New Point(380, 135)
+        inp_tgl.Name = "inp_tgl"
+        inp_tgl.Size = New Size(100, 23)
+        inp_tgl.TabIndex = 3
+        ' 
+        ' tgl
+        ' 
+        tgl.AutoSize = True
+        tgl.Location = New Point(297, 138)
+        tgl.Name = "tgl"
+        tgl.Size = New Size(77, 15)
+        tgl.TabIndex = 2
+        tgl.Text = "Tanggal Lahir"
+        ' 
+        ' ComboBox1
+        ' 
+        ComboBox1.FormattingEnabled = True
+        ComboBox1.Location = New Point(380, 178)
+        ComboBox1.Name = "ComboBox1"
+        ComboBox1.Size = New Size(121, 23)
+        ComboBox1.TabIndex = 5
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Location = New Point(308, 181)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(66, 15)
+        Label2.TabIndex = 6
+        Label2.Text = "Bulan Lahir"
+        ' 
+        ' Button1
+        ' 
+        Button1.Location = New Point(335, 235)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(75, 23)
+        Button1.TabIndex = 7
+        Button1.Text = "Proses"
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' ket
+        ' 
+        ket.AutoSize = True
+        ket.Location = New Point(308, 296)
+        ket.Name = "ket"
+        ket.Size = New Size(19, 15)
+        ket.TabIndex = 8
+        ket.Text = "...."
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(800, 450)
+        Controls.Add(ket)
+        Controls.Add(Button1)
+        Controls.Add(Label2)
+        Controls.Add(ComboBox1)
+        Controls.Add(inp_tgl)
+        Controls.Add(tgl)
+        Controls.Add(inp_name)
+        Controls.Add(Label1)
+        Name = "Form1"
+        Text = "Form1"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents inp_name As TextBox
+    Friend WithEvents inp_tgl As TextBox
+    Friend WithEvents tgl As Label
+    Friend WithEvents ComboBox1 As ComboBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Button1 As Button
+    Friend WithEvents ket As Label
+
+End Class
---
+++ aplikasi_zodiak/aplikasi_zodiak/Form1.vb
@@ -0,0 +1,119 @@
+﻿Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        ComboBox1.Items.Add("Januari")
+        ComboBox1.Items.Add("Februari")
+        ComboBox1.Items.Add("Maret")
+        ComboBox1.Items.Add("April")
+        ComboBox1.Items.Add("Mei")
+        ComboBox1.Items.Add("Juni")
+        ComboBox1.Items.Add("Juli")
+        ComboBox1.Items.Add("Agustus")
+        ComboBox1.Items.Add("September")
+        ComboBox1.Items.Add("Oktober")
+        ComboBox1.Items.Add("November")
+        ComboBox1.Items.Add("Desember")
+    End Sub
+
+    Private Sub inp_tgl_TextChanged(sender As Object, e As EventArgs) Handles inp_tgl.TextChanged
+        inp_tgl.MaxLength = 2
+        If Val(inp_tgl.Text) > 31 Then
+            MsgBox("Tanggal tidak valid")
+            inp_tgl.Text = ""
+        End If
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        If ComboBox1.SelectedItem IsNot Nothing AndAlso Not String.IsNullOrEmpty(inp_tgl.Text) Then
+            Dim bulan As String = ComboBox1.SelectedItem.ToString()
+            Dim tanggal As Integer = Val(inp_tgl.Text)
+            Dim zodiak As String = GetZodiac(bulan, tanggal)
+            ket.Text = "Hai " & inp_name.Text & " ,zodiak kamu adalah " & zodiak
+
+        Else
+            MsgBox("Please select a month and enter a valid date.")
+        End If
+    End Sub
+
+    Private Function GetZodiac(bulan As String, tanggal As Integer) As String
+        Select Case bulan
+            Case "Januari"
+                If tanggal <= 19 Then
+                    Return "Capricorn"
+                Else
+                    Return "Aquarius"
+                End If
+            Case "Februari"
+                If tanggal <= 18 Then
+                    Return "Aquarius"
+                Else
+                    Return "Pisces"
+                End If
+            Case "Maret"
+                If tanggal <= 20 Then
+                    Return "Pisces"
+                Else
+                    Return "Aries"
+                End If
+            Case "April"
+                If tanggal <= 19 Then
+                    Return "Aries"
+                Else
+                    Return "Taurus"
+                End If
+            Case "Mei"
+                If tanggal <= 20 Then
+                    Return "Taurus"
+                Else
+                    Return "Gemini"
+                End If
+            Case "Juni"
+                If tanggal <= 20 Then
+                    Return "Gemini"
+                Else
+                    Return "Cancer"
+                End If
+            Case "Juli"
+                If tanggal <= 22 Then
+                    Return "Cancer"
+                Else
+                    Return "Leo"
+                End If
+            Case "Agustus"
+                If tanggal <= 22 Then
+                    Return "Leo"
+                Else
+                    Return "Virgo"
+                End If
+            Case "September"
+                If tanggal <= 22 Then
+                    Return "Virgo"
+                Else
+                    Return "Libra"
+                End If
+            Case "Oktober"
+                If tanggal <= 22 Then
+                    Return "Libra"
+                Else
+                    Return "Scorpio"
+                End If
+            Case "November"
+                If tanggal <= 21 Then
+                    Return "Scorpio"
+                Else
+                    Return "Sagittarius"
+                End If
+            Case "Desember"
+                If tanggal <= 21 Then
+                    Return "Sagittarius"
+                Else
+                    Return "Capricorn"
+                End If
+            Case Else
+                Return "Invalid"
+        End Select
+    End Function
+
+    Private Sub inp_name_TextChanged(sender As Object, e As EventArgs) Handles inp_name.TextChanged
+
+    End Sub
+End Class
